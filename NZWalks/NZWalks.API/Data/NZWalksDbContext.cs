75cc90ea840fe42cb1518ace218bf7ffc91add79
DannSantana <dangelosantana@hotmail.com>
2024-10-08 20:36:55 -0400

    initial commit

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Model.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOrions):base(dbContextOrions)
+        {
+            
+        }
+        DbSet<Difficulty> Difficulties { get; set; }
+
+        DbSet<Region> Regions { get; set; }
+
+        DbSet<Walk> Walks { get; set; }
+
+    }
+}
---
+++ NZWalks/NZWalks.API/Model/Domain/Walk.cs
@@ -0,0 +1,12 @@
+﻿namespace NZWalks.API.Model.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LenghtKm { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid DifficultyId { get; set; }
+    }
+}

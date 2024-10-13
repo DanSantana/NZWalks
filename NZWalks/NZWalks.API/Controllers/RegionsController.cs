8bedb8a9a562dc658af788f23de708c7fb4062bc
DannSantana <dangelosantana@hotmail.com>
2024-10-13 16:28:33 -0400

    added harcoded data to test the api call. removed https call for while.

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Model.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var regions = new List<Region>();
+                regions.Add(new Region
+                {
+                    Id = new Guid(),
+                    Name = "Rive du sud",
+                    Code = "Richelieu",
+                    RegionImageUrl = "https://maps.app.goo.gl/V8JfTLSd4QpVBtKb9"
+                });
+
+                regions.Add(new Region
+                {
+                    Id = new Guid(),
+                    Name = "Montreal",
+                    Code = "MTL",
+                    RegionImageUrl = "https://maps.app.goo.gl/V8JfTLSd4QpVBtKb9"
+                });
+
+
+                return Ok(regions);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+    }
+}
